203d8759a6c643ed1b104a6ba6cc69894fca3403
root <hariragavan810@gmail.com>
2025-03-07 16:28:48 +0530

    Added init file

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_socketio import SocketIO, emit
 from pymongo import MongoClient
 import datetime
-import config
+from backend import config
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
